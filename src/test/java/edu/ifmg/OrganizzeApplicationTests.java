21881c56b1398be94607d4b59c9ad0d6b4efdaba
Caju Golden <ricardo.alve100@gmail.com>
2023-06-23 14:48:42 -0300

    Initial commit

---
+++ src/test/java/edu/ifmg/OrganizzeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifmg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

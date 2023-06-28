4a6fc914135e510ca130b0360112df69d6c418db
Renan Evilásio <renan.evilasio.97@gmail.com>
2023-06-27 23:55:51 -0300

    Começo Verificação de Limite

--- src/main/java/edu/ifmg/api/controll/FaturaController.java
+++ src/main/java/edu/ifmg/api/controll/FaturaController.java
@@ -8,6 +8,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.sql.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -42,7 +43,6 @@ public class FaturaController {
 
     @DeleteMapping("/{fatura_Id}")
     public ResponseEntity<Fatura> remover(@PathVariable Long faturaId) {
-
         try {
             Optional<Fatura> fatura = faturaRepository.findById(faturaId);
             if (fatura.isPresent()) {
@@ -66,5 +66,35 @@ public class FaturaController {
             return ResponseEntity.ok(faturaSalva);
         }
         return ResponseEntity.notFound().build();
+    }
+
+    public Double verificarLimite(int id_categoria) {
+        Double valorTotal = 0.00;
+
+        String sql = "SELECT sum(vrTotal) AS vrTotal                        "+
+                     "FROM (                                                "+
+                     "  SELECT sum(f.valorTotal) AS vrTotal                 "+
+                     "  FROM fatura f                                       "+
+                     "  WHERE f.faturado = True AND f.categoria_id = ?      "+
+                     "  UNION                                               "+
+                     "  SELECT sum(t.valor) AS vrTotal                      "+
+                     "  FROM fatura f                                       "+
+                     "  INNER JOIN transacao t ON t.fatura_id = f.fatura_id "+
+                     "  WHERE faturado = False AND categoria_id = ?         "+
+                     ")                                                     ";
+        //try {
+            //PreparedStatement ps = conexao.prepareStatement(sql);
+            //ps.setInt(1, id_categoria);
+            //ps.setInt(2, id_categoria);
+            //ResultSet rs = ps.executeQuery();
+            //valorTotal = rs.getDouble("vrTotal");
+            //rs.close();
+            //ps.close();
+
+            return valorTotal;
+
+        //} catch (SQLException e) {
+        //    System.err.println("Erro ao verificar o valor gasto pela categoria: " + e.getMessage());
+        //}
     }
 }

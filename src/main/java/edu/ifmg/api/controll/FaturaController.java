52c5db9099f270f31fb32a8d3f71c2aa694a6dfc
Caju Golden <ricardo.alve100@gmail.com>
2023-06-27 23:54:19 -0300

    salvarTeste()
    inserir uma fatura, e gerar inserts em transacao
    de acordo com a quantidade de fatura.parcelas

--- src/main/java/edu/ifmg/api/controll/FaturaController.java
+++ src/main/java/edu/ifmg/api/controll/FaturaController.java
@@ -1,14 +1,20 @@
 package edu.ifmg.api.controll;
 
 import edu.ifmg.domain.model.Fatura;
+import edu.ifmg.domain.model.Transacao;
 import edu.ifmg.domain.repository.FaturaRepository;
+import edu.ifmg.domain.repository.TransacaoRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,7 +43,52 @@ public class FaturaController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Fatura salvar(@RequestBody Fatura fatura) {
+
         return faturaRepository.save(fatura);
+
+
+
+
+    }
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/insert")
+    public Fatura salvarTeste(@RequestBody Fatura fatura) {
+
+
+        // Salva a fatura
+        Fatura faturaSalva = faturaRepository.save(fatura);
+
+        // Gera as transações
+        List<Transacao> transacoes = new ArrayList<>();
+        for (int i = 1; i <= fatura.getParcelas(); i++) {
+            Transacao transacao = new Transacao();
+            transacao.setValor((long) (fatura.getValorTotal() / fatura.getParcelas()));
+            transacao.setParcela((long) i);
+            transacao.setFatura(faturaSalva);
+            transacoes.add(transacao);
+            System.out.println("quero isso: " + transacao);
+        }
+
+        // Salva as transações no banco de dados usando instruções SQL
+        for (Transacao transacao : transacoes) {
+            String sql = "INSERT INTO transacao (data_pagamento, data_transacao, data_vencimento, parcela, valor, fatura_id) VALUES (?, ?, ?, ?, ?, ?)";
+            Object[] params = {
+                    null,
+                    LocalDateTime.now(),
+                    LocalDateTime.now().plusDays(30),
+                    transacao.getParcela(),
+                    transacao.getValor(),
+                    transacao.getFatura().getId()
+            };
+            jdbcTemplate.update(sql, params);
+        }
+
+        return faturaSalva;
+
     }
 
     @DeleteMapping("/{fatura_Id}")
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,4 +6,4 @@ INSERT INTO meta_categoria(limite,controle,categoria_id) VALUES (2,1,1);
 
 INSERT INTO fatura(faturado,parcelas,valor_total,categoria_id) VALUES (true,3,250.00,1);
 
-INSERT INTO transacao(data_pagamento,data_transacao,data_vencimento,parcela,valor,fatura_id) VALUES (utc_timestamp,DATE_ADD(CURDATE(), INTERVAL 30 DAY),DATE_ADD(CURDATE(), INTERVAL 45 DAY),3,250.00,1);
+--INSERT INTO transacao(data_pagamento,data_transacao,data_vencimento,parcela,valor,fatura_id) VALUES (utc_timestamp,DATE_ADD(CURDATE(), INTERVAL 30 DAY),DATE_ADD(CURDATE(), INTERVAL 45 DAY),3,250.00,1);
